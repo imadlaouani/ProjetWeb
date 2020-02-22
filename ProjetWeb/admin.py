fac20c764c0e17671031a34e20ae915091ceb52f
Laouani <imad.laouani@etu.umontpellier.fr>
2020-02-22 23:41:14 +0100

    admin

---
+++ ProjetWeb/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from ProjetWeb.models import *
+
+admin.site.register(Categorie)
+admin.site.register(Produit)
+admin.site.register(Vendeur)
+admin.site.register(Acheteur)
+admin.site.register(Enchere)
--- ProjetWeb/models.py
+++ ProjetWeb/models.py
@@ -2,42 +2,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from PIL import Image
 
-class Produit(models.Model):
-    numProduit = models.AutoField(primary_key=True)
-    titre = models.CharField(max_length=30)
-    desciption = models.TextField()
-    #images = ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)
-    prixBase = DecimalField(max_digits=6, decimal_places=1)
-    dateDebut = models.DateTimeField(default=timezone.now, verbose_name="Date de début de l'enchère")
-    dateFin = models.DateTimeField(verbose_name="Date de fin de l'enchère")
-    vendeurFK = models.ForeignKey(Vendeur, on_delete=models.CASCADE, db_column='numVendeur')
-    categorieFK = models.ForeignKey(Categorie, on_delete=models.CASCADE, db_column='numCategorie')
 
-    def __str__(self):
-        return self.nom
+
 
 class Vendeur(models.Model):
     numVendeur = models.AutoField(primary_key=True)
     nomVendeur = models.CharField(max_length=30)
     prenomVendeur = models.CharField(max_length=30)
     mailVendeur = models.EmailField()
-    produits = models.ManyToManyField(Produit, through='Offre', related_name='+')
     noteVendeur = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
-    
+    class Meta:
+        db_table ='vendeur'
 
     def __str__(self):
         return self.nomVendeur
 
-"""class Offre(models.Model):
-    prix = models.IntegerField()
-    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
-    vendeur = models.ForeignKey(Vendeur, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return "{0} vendu par {1}".format(self.produit, self.vendeur)*/"""
+
 
 
 class Acheteur(models.Model):
@@ -62,10 +44,24 @@ class Categorie(models.Model):
         ('Maison', 'Autre')
     )
     numCategorie = models.AutoField(primary_key=True)
-    libelleCategorie = models.CharField(blank=True, max_length=5, choices=Categorie_choices)
+    libelleCategorie = models.CharField(blank=True, max_length=30, choices=Categorie_choices)
 
     def __str__(self):
         return self.libelleCategorie
+
+class Produit(models.Model):
+    numProduit = models.AutoField(primary_key=True)
+    titre = models.CharField(max_length=30)
+    desciption = models.TextField()
+    images = models.    ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    prixBase =  models.FloatField(validators=[MinValueValidator(0)])
+    dateDebut = models.DateTimeField(default=timezone.now, verbose_name="Date de début de l'enchère")
+    dateFin = models.DateTimeField(verbose_name="Date de fin de l'enchère")
+    vendeurFK = models.ForeignKey(Vendeur, on_delete=models.CASCADE, db_column='numVendeur')
+    categorieFK = models.ForeignKey(Categorie, on_delete=models.CASCADE, db_column='numCategorie')
+
+    def __str__(self):
+        return self.nom
 
 
 class Enchere(models.Model):
